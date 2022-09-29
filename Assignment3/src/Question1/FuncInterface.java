ec2ec7aba00dd4effff91d733a5f1e276b381e96
NidhishreeLA2 <nidhishree.la@kpipartners.com>
2022-09-30 00:23:55 +0530

    c

--- Assignment3/src/Question1/FuncInterface.java
+++ Assignment3/src/Question1/FuncInterface.java
@@ -1,11 +1,23 @@
 package Question1;
 
 // Functional Interface has only one abstract method
-public interface FuncInterface {
+interface FuncInterface {
 	
-	void display();
+	int twice(int a);
 
 }
 
+class Test{
+	public static void main(String args[]) {
+		int z = 10;
+		
+		FuncInterface f = (int a) -> a*a;
+		
+		int ans = f.twice(z);
+		System.out.println(ans);
+				
+	}
+	
+}
 
 	
